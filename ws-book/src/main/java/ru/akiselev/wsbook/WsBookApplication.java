19a67eb3d27a3bbba2809f52dd780f1e87d47fee
Киселев Алексей Владиславович <AKiselev5@alfabank.ru>
2025-03-11 18:04:27 +0300

    микросервисы чутьчтуь

---
+++ ws-book/src/main/java/ru/akiselev/wsbook/WsBookApplication.java
@@ -0,0 +1,13 @@
+package ru.akiselev.wsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsBookApplication.class, args);
+    }
+
+}
---
+++ ws-book/src/test/java/ru/akiselev/wsbook/WsBookApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akiselev.wsbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
